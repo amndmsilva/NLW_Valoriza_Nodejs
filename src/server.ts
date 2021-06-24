54002d568452a134faeaa49777a53e16fedb2276
Amanda Santos <amndmsilva@gmail.com>
2021-06-24 17:55:25 -0300

    class 2

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import "reflect-metadata";
-import express, { response } from "express";
+import express from "express";
+import { router } from "./routes";
+
+import "./database";
 
 //@types/express
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 
 //http://localhost:3000
